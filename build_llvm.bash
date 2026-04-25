996a7baa1b32d655dee91eb47c3c183792a5ad81
Maverick Long <maverick@minasan.xyz>
2026-04-25 17:21:45 +0000

    refactor: add LLVM / TIM-VX dependencies; added build boilerplate; updated license to Apache 2.0 \w LLVM exceptions

---
+++ build_llvm.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+PROJECT_ROOT=$(dirname "$(realpath "$0")")
+mkdir -p llvm-project/build
+cd llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=ON \
+   -DLLVM_TARGETS_TO_BUILD="Native" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON \
+   -DLLVM_CCACHE_BUILD=ON \
+   -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+   -DPython3_EXECUTABLE="$PROJECT_ROOT/.venv/bin/python" \
+   -DCMAKE_C_VISIBILITY_PRESET=hidden \
+   -DCMAKE_CXX_VISIBILITY_PRESET=hidden \
+   -DCMAKE_VISIBILITY_INLINES_HIDDEN=ON \
+   -DMLIR_BINDINGS_PYTHON_NB_DOMAIN=timvx
+cmake --build . --target check-mlir
